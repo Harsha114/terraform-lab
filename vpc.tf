81940cafcceb903f8dc6e5c15b0024c30d200487
PILLI HOSHAYYA <60870775+Harsha114@users.noreply.github.com>
2020-08-26 14:33:16 +0530

    vpc creation

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIA3YVPSDEKQGZLJAY2"
-  secret_key = "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"
+  access_key = "Access-id"
+  secret_key = "Secret-id"
 }
 resource "aws_vpc" "demoVPC" {
   cidr_block       = "10.0.0.0/16"
